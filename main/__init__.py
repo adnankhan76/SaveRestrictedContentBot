9cbf6040a2b74a55f5e540da1cff03baff5843f9
adnankhan76 <150260782+adnankhan76@users.noreply.github.com>
2024-04-25 23:14:20 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,13 +12,6 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 29721235
-API_HASH = "a0045602d212e62001bd1ae234b04043"
-BOT_TOKEN = "7137774275:AAEeH4xtB89EUiHwnCZRVXIgathLP3-mqXk"
-SESSION = "1BVtsOK0Bu2HLagVIndP89xPEPEY22iCKuYzOe5KsrnfQQ-HQw9XE_xakv_I6vc6wsim_v7X2BMC9hEXjhL-Avo8Yketw3YS9HUwM9zCOizVI_QC4J36w0OIWNojVd3AThc96reHQ_NZc7rnkRQsCQQdkQTpCWdkny6_7dZaUzOYHtQuAcb2B-wuUXBkDJ36N2hdYiC3oUvpNeC0c3dXEomN1J_8w-ChxCklZJhEjRFRVW7JyRs_QE05VhGncKNitd9RJcDD6kcdlkGZFKQtpyOO44nQbD_Xt6wCnzPIkqtaYCsECI_EaSiaiPkZNF6gQUIj3K7gCDjm-lWHEd-LmYeozU7V4wa4="
-FORCESUB = LOGLOGLOGAD
-AUTH = 1665996635
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
 userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
 
